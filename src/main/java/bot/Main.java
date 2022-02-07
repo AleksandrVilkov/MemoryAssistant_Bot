7f3d2ee3d6a4b3ef970ea59f1ae3661031ae460f
Vilkov Aleksandr <vilkovaleksandr@gmail.com>
2022-02-07 10:14:12 +0300

    update

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -7,21 +7,20 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    //private static final Logger logger = Logger.getLogger(Main.class);
+    private static final Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-//        logger.info("Start my app");
-//        TelegramBotsApi botsApi;
-//        try {
-//            logger.info("Trying to register a bot");
-//            botsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            botsApi.registerBot(new UserInteraction());
-//            logger.info("Bot successfully created!");
-//        } catch (TelegramApiException e) {
-//            e.printStackTrace();
-//            logger.error("ERROR! Failed to create bot!");
-//        }
+        logger.info("Start my app");
+        TelegramBotsApi botsApi;
+        try {
+            logger.info("Trying to register a bot");
+            botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new UserInteraction());
+            logger.info("Bot successfully created!");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            logger.error("ERROR! Failed to create bot!");
+        }
     }
 
 
